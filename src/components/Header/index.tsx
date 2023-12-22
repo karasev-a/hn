383eebdd6fa6c9a7057cd6d5ad1fe59d13136095
o_karasov <karasev.avk@gmail.com>
2023-12-22 08:16:09 +0000

    add prettier, folders structure.

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./header.module.scss";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <p>Header</p>
+    </header>
+  );
+};
+
+export default Header;
