5f33e2995673a2e82e94247efac6a22ab27ab687
o_karasov <karasev.avk@gmail.com>
2023-12-27 18:37:26 +0000

    add getDateTime util

---
+++ src/utils/getDateTime.ts
@@ -0,0 +1,14 @@
+export const getDateTime = (time: number) => {
+  const options = {
+    year: "numeric" as const,
+    month: "numeric" as const,
+    day: "numeric" as const,
+    hour: "numeric" as const,
+    minute: "numeric" as const,
+    second: "numeric" as const,
+    hour12: false,
+    timeZone: "America/Los_Angeles" as const,
+  };
+
+  return new Intl.DateTimeFormat(undefined, options).format(time);
+};
