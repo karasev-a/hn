203f88e7e3e6f1c8f41c0c07d20fbda9112ad00c
o_karasov <karasev.avk@gmail.com>
2023-12-31 16:59:47 +0000

    add some styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <p>Hacker News</p>
+        <p>HN</p>
       </div>
       <NavBar />
       <div className={styles.userAction}>
